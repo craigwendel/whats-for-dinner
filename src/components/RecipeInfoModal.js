76f980f489c88a91bd416a453e4d2b40d6ee1691
Craig Wendel <wendelcraig@gmail.com>
2017-09-12 13:58:56 -0500

    Add iframe in modal, need to map unique URL in modal.

--- src/components/RecipeInfoModal.js
+++ src/components/RecipeInfoModal.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import Modal from 'react-responsive-modal'
 
 export default class RecipeInfoModal extends Component {
@@ -13,23 +13,19 @@ export default class RecipeInfoModal extends Component {
   }
 
   onOpenModal () {
-    this.setState({ open: true })
+    this.setState({open: true})
   }
   onCloseModal () {
-    this.setState({ open: false })
+    this.setState({open: false})
   };
 
   render () {
-    const { open } = this.state
+    const {open} = this.state
     return (
       <div className='recipe-info-modal'>
         <button onClick={this.onOpenModal}>More Info</button>
         <Modal open={open} onClose={this.onCloseModal} modalClassName='modal'>
-          <h2>Title:</h2>
-          <h4>Info:</h4>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          </p>
-          <button>Button</button>
+          <iframe src='http://food2fork.com/view/To_Die_For_Blueberry_Muffins/32745' width='1000' height='550'></iframe>
         </Modal>
       </div>
     )
